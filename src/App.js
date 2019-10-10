afe22c83ded026cad53f0bff083897fda37900ee
Juanru <yz5026@nyu.edu>
2019-10-10 11:45:17 +0800

    format issue fixed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import './App.css';
-import News from './News';
-
+import React from "react";
+import "./App.css";
+import News from "./News";
 
 function App() {
-  return (
-    <div>
-      <News date="Oct. 8th, 2019" header="NYU Shanghai students suffer from midterm" des="Perspective on Humanities/IMA Projects/Club Issues..." author="Xi" newspic="https://bit.ly/2xTmiug"/>
-      <News date="Oct. 8th, 2019" header="NYU Shanghai students suffer from midterm" des="Literally" author="Xi" newspic="https://bit.ly/2K4kdEV"/>
-      <News date="Oct. 8th, 2019" header="NYU Shanghai students suffer from midterm" des="Literally" author="Xi" newspic="https://bit.ly/2K4kdEV"/>
-    </div>
-  );
+    return (<div>
+        <News date="Oct. 8th, 2019"
+            header="NYU Shanghai students are greatly suffering from midterm"
+            des="Perspective on Humanities/IMA Projects/Club Issues..."
+            author="Juanru"
+            newspic="https://bit.ly/2xTmiug" /
+        >
+        <News date="Oct. 8th, 2019"
+            header="NYU Shanghai students suffer from midterm"
+            des="Literally"
+            author="Xi"
+            newspic="https://bit.ly/2xTmiug" /
+        >
+        <News date="Oct. 8th, 2019"
+            header="NYU Shanghai students suffer from midterm"
+            des="Literally"
+            author="Xi"
+            newspic="https://bit.ly/2xTmiug" />
+        </div>
+    );
 }
 
-
-export default App;
+export default App; 
--- src/News.js
+++ src/News.js
@@ -1,41 +1,28 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function News(props) {
-    return (
-        <div className="styleOfAll">
-
-            <div className="date">
-                <p>{props.date}</p>
-            </div>
-
-
+    return ( 
+        <div className = "styleOfAll" >
+            <div className = "date" >
+                <p > { props.date } </p> 
+            </div> 
             <div>
-                <div className="HeadofNews">
-                    <p>{props.header}</p>
-                </div>
-                <div className="desciption">
-                    <p>{props.des}</p>
-                </div>
-                <div className="author">
-                    <p>
-                        By {props.author}
-                    </p>
-                </div>
-            </div>
-
-
-
-            <div className="NewsPic">
-                <img width="100%" src={props.newspic} />
-            </div>
+                <div className = "HeadofNews" >
+                    <p> { props.header } </p> 
+                </div> 
+                <div className = "description" >
+                    <p> { props.des } </p> 
+                </div> 
+                <div className = "author" >
+                    <p> By { props.author } </p> 
+                </div> 
+            </div> 
+            <div className = "NewsPic" >
+                <img src = { props.newspic }/> 
+            </div> 
         </div>
-
-
-
     );
-
 }
-
 
 export default News;
