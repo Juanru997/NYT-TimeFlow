0e626869d61e7a80b15aa7ef0e079b5e12d4a9af
Juanru <yz5026@nyu.edu>
2019-10-14 14:53:52 +0800

    flexbox issues fixed

--- src/App.js
+++ src/App.js
@@ -8,20 +8,20 @@ function App() {
             header="NYU Shanghai students are greatly suffering from midterm"
             des="Perspective on Humanities/IMA Projects/Club Issues..."
             author="Juanru"
-            newspic="https://bit.ly/2xTmiug" /
+            newspic="https://bit.ly/2VGiEiW" /
         >
         <News date="Oct. 8th, 2019"
-            header="NYU Shanghai students suffer from midterm"
-            des="Literally"
-            author="Xi"
-            newspic="https://bit.ly/2xTmiug" /
+            header="Greta Thunberg Won the Nobel Peace Price"
+            des="The Norwegian Nobel Committee has decided to award the Nobel Peace Prize for 2019 to Greta Thunberg."
+            author="James Zhu"
+            newspic="https://bit.ly/2pomP6L" /
         >
         <News date="Oct. 8th, 2019"
-            header="NYU Shanghai students suffer from midterm"
-            des="Literally"
-            author="Xi"
-            newspic="https://bit.ly/2xTmiug" />
-        </div>
+            header="NYU Students Claim to Have Invented a Robot for Auto Web-Coding"
+            des="'I wish in the future people won't need to do the dirty work of CSS, so I hope I could find a efficient way to create a website', said by a NYU Shanghai student who is greatly suffering from non-sense CSS work"
+            author="Yanru"
+            newspic="https://bit.ly/2IPXNo5" />
+    </div>
     );
 }
 
--- src/News.js
+++ src/News.js
@@ -2,25 +2,25 @@ import React from "react";
 import "./App.css";
 
 function News(props) {
-    return ( 
-        <div className = "styleOfAll" >
-            <div className = "date" >
-                <p > { props.date } </p> 
-            </div> 
-            <div>
-                <div className = "HeadofNews" >
-                    <p> { props.header } </p> 
-                </div> 
-                <div className = "description" >
-                    <p> { props.des } </p> 
-                </div> 
-                <div className = "author" >
-                    <p> By { props.author } </p> 
-                </div> 
-            </div> 
-            <div className = "NewsPic" >
-                <img src = { props.newspic }/> 
-            </div> 
+    return (
+        <div className="styleOfAll" >
+            <div className="date" >
+                <p > {props.date} </p>
+            </div>
+            <div className="mainPart">
+                <div className="HeadofNews" >
+                    <p> {props.header} </p>
+                </div>
+                <div className="description" >
+                    <p> {props.des} </p>
+                </div>
+                <div className="author" >
+                    <p> By {props.author} </p>
+                </div>
+            </div>
+            <div className="NewsPic" >
+                <img src={props.newspic} />
+            </div>
         </div>
     );
 }
